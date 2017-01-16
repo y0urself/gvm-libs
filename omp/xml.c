e55805736344a0a6d8bb41586628e73a7801d337
Hani Benhabiles <hani.benhabiles@greenbone.net>
2017-01-16 12:55:46 +0000

    * omp/xml.c (try_read_entity_and_string_s): Fix memory leak.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/openvas-libraries@27220 423fd1db-d629-0410-8442-d21db03e70f4

--- omp/xml.c
+++ omp/xml.c
@@ -780,6 +780,7 @@ try_read_entity_and_string_s (int socket, int timeout, entity_t *entity,
             *string_return = string;
           if (timeout > 0)
             fcntl (socket, F_SETFL, 0L);
+          g_slist_free (context_data.first);
           g_markup_parse_context_free (xml_context);
           return 0;
         }
